c18fa63f3c5fadfb76c982e42fd4b42492dcb406
Pablo Querales <pabloquerales20@gmail.com>
2025-04-12 16:45:29 +0200

    se creó la estructura base del proyecto y se consumió de manera exitosa la API

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,58 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export default function Home() {
+	const audioRef = useRef("/sound/files/mario/songs/castle.mp3");
+	const [song, setSong] = useState([]);
 
-//create your first component
-const Home = () => {
+	const getAllSong = async () => {
+		const requestOptions = {
+			method: "GET",
+			redirect: "follow"
+		};
+
+		try {
+			const response = await fetch("https://playground.4geeks.com/sound/songs", requestOptions);
+			const result = await response.json();
+			setSong(result.songs);
+			if (response.status === 200) {
+				console.log(
+					song.map((song) => {
+						return song.name;
+					})
+				);
+				console.log(
+					song.map((song) => {
+						return song.url;
+					})
+				);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	useEffect(() => {
+		getAllSong();
+	}, []);
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container mt-5">
+				<div className="header d-flex justify-content-center">
+					<h2>Sound-API</h2>
+				</div>
+				<div className="container">
+					{song.map((song) => {
+						return <p>{song.name}</p>;
+					})}
+				</div>
+			</div>
+			<button
+				onClick={() => {
+					getAllSong();
+				}}>
+				{" "}
+				hola
+			</button>
+			<audio ref={audioRef} controls></audio>
+		</>
 	);
-};
-
-export default Home;
+}
